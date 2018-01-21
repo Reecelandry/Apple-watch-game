c9b0c61a31fbc4fdaf59b1c3209c743c657825c1
Reece Landry <2002rlandry@gmail.com>
2018-01-21 14:08:11 -0800

    Created Apple Watch Game 3 Project

---
+++ Apple Watch Game 3/Apple Watch Game 3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Apple Watch Game 3
+//
+//  Created by Reece Landry on 2018-01-21.
+//  Copyright © 2018 Reece Landry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
